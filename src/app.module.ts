ddafb52c4ac99084ebf97995dcb0a63a1a2f9955
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-06 18:28:33 -0300

    feat(app): setup typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
+import { BankAccountEntity } from './bank-accounts/entities/bank-account.entity';
 
 @Module({
-  imports: [BankAccountsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: __dirname + '/db.sqlite',
+      synchronize: true,
+      logging: true,
+      entities: [
+        BankAccountEntity
+      ]
+    }),
+    BankAccountsModule
+  ],
   controllers: [],
   providers: [],
 })
