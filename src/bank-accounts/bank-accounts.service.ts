62b85712eeb78e5b0a596e6fcff2b839d5b0ce34
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-06 19:17:19 -0300

    feat(bankaccounts/service): inject typeorm repository
    - enhance its methods

--- src/bank-accounts/bank-accounts.service.ts
+++ src/bank-accounts/bank-accounts.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
-import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
+
+import { BankAccountEntity } from './entities/bank-account.entity';
 
 @Injectable()
 export class BankAccountsService {
-  create(createBankAccountDto: CreateBankAccountDto): string {
-    return 'This action adds a new bankAccount';
+  constructor(
+    @InjectRepository(BankAccountEntity)
+    private repository: Repository<BankAccountEntity>
+  ) {};
+
+  async create({ account_number }: CreateBankAccountDto): Promise<BankAccountEntity> {
+    const bankAccount: BankAccountEntity = this.repository.create({
+      account_number,
+      balance: 0
+    });
+
+    await this.repository.insert(bankAccount);
+
+    return bankAccount;
   }
 
-  findAll(): string {
-    return `This action returns all bankAccounts`;
+  async findAll(): Promise<BankAccountEntity[]> {
+    return this.repository.find();
   }
 
-  findOne(id: number): string {
-    return `This action returns a #${id} bankAccount`;
-  }
-
-  update(id: number, updateBankAccountDto: UpdateBankAccountDto): string {
-    return `This action updates a #${id} bankAccount`;
-  }
-
-  remove(id: number): string {
-    return `This action removes a #${id} bankAccount`;
+  async findOne(id: string): Promise<BankAccountEntity> {
+    return this.repository.findOneBy({ id });
   }
 }
