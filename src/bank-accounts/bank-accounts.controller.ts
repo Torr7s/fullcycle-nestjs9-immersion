62ad67a917b36627c1ff9aa91986a47f51eb08fd
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-06 19:18:09 -0300

    feat(bankaccounts/controller): enhance its methods

--- src/bank-accounts/bank-accounts.controller.ts
+++ src/bank-accounts/bank-accounts.controller.ts
@@ -1,34 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { 
+  Controller, 
+  Get, 
+  Post, 
+  Body, 
+  Param, 
+} from '@nestjs/common';
+
 import { BankAccountsService } from './bank-accounts.service';
+
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
-import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
+
+import { BankAccountEntity } from './entities/bank-account.entity';
 
 @Controller('bank-accounts')
 export class BankAccountsController {
   constructor(private readonly bankAccountsService: BankAccountsService) {}
 
   @Post()
-  create(@Body() createBankAccountDto: CreateBankAccountDto): string {
+  create(@Body() createBankAccountDto: CreateBankAccountDto): Promise<BankAccountEntity> {
     return this.bankAccountsService.create(createBankAccountDto);
   }
 
   @Get()
-  findAll(): string {
+  findAll(): Promise<BankAccountEntity[]> {
     return this.bankAccountsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): string {
-    return this.bankAccountsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBankAccountDto: UpdateBankAccountDto): string {
-    return this.bankAccountsService.update(+id, updateBankAccountDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string): string {
-    return this.bankAccountsService.remove(+id);
+  findOne(@Param('id') id: string): Promise<BankAccountEntity> {
+    return this.bankAccountsService.findOne(id);
   }
 }
