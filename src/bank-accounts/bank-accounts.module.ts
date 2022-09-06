230bc339b7a9883e5c9ac2e21af2a206c56324c7
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-06 19:19:43 -0300

    feat(bankaccounts): setup typeorm module
    use forFeature to define which repositories are registered in the current scope

--- src/bank-accounts/bank-accounts.module.ts
+++ src/bank-accounts/bank-accounts.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { BankAccountsService } from './bank-accounts.service';
 import { BankAccountsController } from './bank-accounts.controller';
+import { BankAccountEntity } from './entities/bank-account.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      BankAccountEntity
+    ])
+  ],
   controllers: [BankAccountsController],
   providers: [BankAccountsService]
 })
