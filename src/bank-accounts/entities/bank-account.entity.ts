e7c9f5294392c57d57cd932be9e4aa8678d8bec2
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-06 18:28:17 -0300

    feat(entities): add properties and decorate it properly

--- src/bank-accounts/entities/bank-account.entity.ts
+++ src/bank-accounts/entities/bank-account.entity.ts
@@ -1,1 +1,13 @@
-export class BankAccount {};
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'accounts'})
+export class BankAccountEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'decimal', precision: 2 })
+  balance: number;
+
+  @Column({ length: 255 })
+  account_number: string;
+};
