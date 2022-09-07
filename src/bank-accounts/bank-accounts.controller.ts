d237fbcc7757a55de72d1d57ab290d6e5361947c
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-07 10:22:50 -0300

    feat(bankaccounts): create transfer controller

--- src/bank-accounts/bank-accounts.controller.ts
+++ src/bank-accounts/bank-accounts.controller.ts
@@ -3,12 +3,14 @@ import {
   Get, 
   Post, 
   Body, 
-  Param, 
+  Param,
+  HttpCode, 
 } from '@nestjs/common';
 
 import { BankAccountsService } from './bank-accounts.service';
 
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
+import { TransferBankAccountDto } from './dto/transfer-bank-account.dto';
 
 import { BankAccountEntity } from './entities/bank-account.entity';
 
@@ -29,5 +31,15 @@ export class BankAccountsController {
   @Get(':id')
   findOne(@Param('id') id: string): Promise<BankAccountEntity> {
     return this.bankAccountsService.findOne(id);
+  }
+
+  @HttpCode(204)
+  @Post('transfer')
+  transfer(@Body() transferDto: TransferBankAccountDto): Promise<void> {
+    return this.bankAccountsService.transfer(
+      transferDto.from,
+      transferDto.to,
+      transferDto.amount
+    );
   }
 }
